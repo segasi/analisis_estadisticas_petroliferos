74b4f0ed7163d1ba3219eade3a636904f4ebfcb2
segasi <segasi@gmail.com>
2019-01-10 16:44:25 -0600

    Tema gráficas

--- 02_codigo/analisis_datos_semanales.R
+++ 02_codigo/analisis_datos_semanales.R
@@ -6,3 +6,19 @@ p_load(ggrepel, janitor, scales, readxl, tidyverse, treemapify, wesanderson)
 ### Setup ----
 Sys.setlocale("LC_ALL", "es_ES.UTF-8") # Cambiar locale para prevenir problemas con caracteres especiales
 options(scipen=999) # Prevenir notación científica
+
+
+### Definir tema de gráficas ----
+tema <-  theme_minimal() +
+  theme(text = element_text(family="Didact Gothic Regular", color = "grey35"),
+        plot.title = element_text(size = 24, face = "bold", margin = margin(10,0,20,0), family="Trebuchet MS Bold", color = "grey25"),
+        plot.subtitle = element_text(size = 16, face = "bold", colour = "#666666", margin = margin(0, 0, 20, 0), family="Didact Gothic Regular"),
+        plot.caption = element_text(hjust = 0, size = 15),
+        panel.grid = element_line(linetype = 2), 
+        panel.grid.minor = element_blank(),
+        legend.position = "bottom",
+        legend.title = element_text(size = 16, face = "bold", family="Trebuchet MS Bold"),
+        legend.text = element_text(size = 14, family="Didact Gothic Regular"),
+        legend.title.align = 0.5,
+        axis.title = element_text(size = 18, hjust = 1, face = "bold", margin = margin(0,0,0,0), family="Didact Gothic Regular"),
+        axis.text = element_text(size = 16, face = "bold", family="Didact Gothic Regular"))
