f0b25a45b929f849939c8d4bc47bcdae2abf0b7a
segasi <segasi@gmail.com>
2019-01-10 16:43:51 -0600

    Setup

--- 02_codigo/analisis_datos_semanales.R
+++ 02_codigo/analisis_datos_semanales.R
@@ -1,3 +1,8 @@
 ### Paquetes ----
 library(pacman)
 p_load(ggrepel, janitor, scales, readxl, tidyverse, treemapify, wesanderson)
+
+
+### Setup ----
+Sys.setlocale("LC_ALL", "es_ES.UTF-8") # Cambiar locale para prevenir problemas con caracteres especiales
+options(scipen=999) # Prevenir notación científica
