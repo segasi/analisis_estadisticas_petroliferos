85aa6694a9a8861aaa0af2ef16fc7040a429e3c1
segasi <segasi@gmail.com>
2019-01-11 10:28:23 -0600

    Agregar tema para heatmaps

--- 02_codigo/analisis_datos_semanales.R
+++ 02_codigo/analisis_datos_semanales.R
@@ -23,6 +23,20 @@ tema <-  theme_minimal() +
         axis.title = element_text(size = 18, hjust = 1, face = "bold", margin = margin(0,0,0,0), family="Didact Gothic Regular"),
         axis.text = element_text(size = 16, face = "bold", family="Didact Gothic Regular"))
 
+tema_hm <-  theme_minimal() +
+  theme(text = element_text(family="Didact Gothic Regular", color = "grey35"),
+        plot.title = element_text(size = 24, face = "bold", margin = margin(10,0,20,0), family="Trebuchet MS Bold", color = "grey25"),
+        plot.subtitle = element_text(size = 16, face = "bold", colour = "#666666", margin = margin(0, 0, 20, 0), family="Didact Gothic Regular"),
+        plot.caption = element_text(hjust = 0, size = 15),
+        panel.grid = element_line(linetype = 2), 
+        panel.grid.minor = element_blank(),
+        legend.position = "bottom",
+        legend.title = element_text(size = 16, face = "bold", family="Trebuchet MS Bold"),
+        legend.text = element_text(size = 14, family="Didact Gothic Regular"),
+        legend.title.align = 0.5,
+        axis.title = element_text(size = 18, hjust = 1, face = "bold", margin = margin(0,0,0,0), family="Didact Gothic Regular"),
+        axis.text = element_text(size = 12, face = "bold", family="Didact Gothic Regular"))
+
 ### Importar datos ----
 
 # Fuente: http://estadisticashidrocarburos.energia.gob.mx/Datos_semana.aspx
