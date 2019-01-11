8dac19226458f3f8d38c4a866ed185f915f4da02
segasi <segasi@gmail.com>
2019-01-11 12:06:58 -0600

    Cambios varios en el tema_hm

--- 02_codigo/analisis_datos_semanales.R
+++ 02_codigo/analisis_datos_semanales.R
@@ -25,17 +25,21 @@ tema <-  theme_minimal() +
 
 tema_hm <-  theme_minimal() +
   theme(text = element_text(family="Didact Gothic Regular", color = "grey35"),
-        plot.title = element_text(size = 24, face = "bold", margin = margin(10,0,20,0), family="Trebuchet MS Bold", color = "grey25"),
-        plot.subtitle = element_text(size = 16, face = "bold", colour = "#666666", margin = margin(0, 0, 20, 0), family="Didact Gothic Regular"),
-        plot.caption = element_text(hjust = 0, size = 15),
+        plot.title = element_text(size = 32, face = "bold", margin = margin(10,0,20,0), family="Trebuchet MS Bold", color = "grey25"),
+        plot.subtitle = element_text(size = 25, face = "bold", colour = "#666666", margin = margin(0, 0, 20, 0), family="Didact Gothic Regular"),
+        plot.caption = element_text(hjust = 0, size = 25),
         panel.grid = element_line(linetype = 2), 
         panel.grid.minor = element_blank(),
-        legend.position = "bottom",
-        legend.title = element_text(size = 16, face = "bold", family="Trebuchet MS Bold"),
-        legend.text = element_text(size = 14, family="Didact Gothic Regular"),
+        legend.position = c(0.88, -0.12),
+        legend.direction = "horizontal",
+        legend.title = element_text(size = 20, face = "bold", family="Trebuchet MS Bold"),
+        legend.text = element_text(size = 18, family="Didact Gothic Regular"),
         legend.title.align = 0.5,
+        strip.background = element_rect(color = "grey60", fill = "grey60"),
+        strip.text = element_text(color = "white", size = 20),
         axis.title = element_text(size = 18, hjust = 1, face = "bold", margin = margin(0,0,0,0), family="Didact Gothic Regular"),
-        axis.text = element_text(size = 12, face = "bold", family="Didact Gothic Regular"))
+        axis.text.x = element_text(size = 16, face = "bold", family="Didact Gothic Regular"),
+        axis.text.y = element_text(size = 12, face = "bold", family="Didact Gothic Regular"))
 
 ### Importar datos ----
 
