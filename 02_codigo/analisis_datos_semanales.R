a47c061db7275c3b645692334103bc8228146c69
segasi <segasi@gmail.com>
2019-01-11 15:21:19 -0600

    Cambio tema gráfica

--- 02_codigo/analisis_datos_semanales.R
+++ 02_codigo/analisis_datos_semanales.R
@@ -11,7 +11,7 @@ options(scipen=999) # Prevenir notación científica
 ### Definir tema de gráficas ----
 tema <-  theme_minimal() +
   theme(text = element_text(family="Didact Gothic Regular", color = "grey35"),
-        plot.title = element_text(size = 24, face = "bold", margin = margin(10,0,20,0), family="Trebuchet MS Bold", color = "grey25"),
+        plot.title = element_text(size = 28, face = "bold", margin = margin(10,0,20,0), family="Trebuchet MS Bold", color = "grey25"),
         plot.subtitle = element_text(size = 16, face = "bold", colour = "#666666", margin = margin(0, 0, 20, 0), family="Didact Gothic Regular"),
         plot.caption = element_text(hjust = 0, size = 15),
         panel.grid = element_line(linetype = 2), 
